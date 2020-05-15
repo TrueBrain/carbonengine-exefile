fa452ff55539f4276a944800c9ba77dbf9690bbe
CCP Aporia <28982391+CCP-Aporia@users.noreply.github.com>
2020-05-15 17:31:16 +0000

    Merge using //eve/development/MAINLINE->//eve/sandbox/PER-BRANCH-SDK-FOLDER
    This brings in the vendored SDK work
    https://jira.ccpgames.com/browse/TTC-1025
    
    [git-p4: depot-paths = "//eve/branches/development/MAINLINE/carbon/src/Exefile/": change = 1727524]

--- RegenerateProjectFile.cmd
+++ RegenerateProjectFile.cmd
@@ -1,7 +1,7 @@
 @echo off
 echo Checking out project and filters file
-p4 edit ExeFile.vcxproj
-p4 edit ExeFile.vcxproj.filters
+p4 edit ExeFile_v141.vcxproj
+p4 edit ExeFile_v141.vcxproj.filters
 echo Regenerating
-..\..\..\..\..\..\shared_tools\python\27\python.exe ..\..\tools\ProjectFileGenerator\ProjectFileGenerator.py -i ExeFile.ccpproj
+..\..\..\..\..\..\shared_tools\python\27\python.exe ..\..\tools\ProjectFileGenerator\ProjectFileGenerator.py -i ExeFile.ccpproj --toolset=v141 --outfile=ExeFile_v141.vcxproj
 pause
