91ee6d3f142919e239c23a80ba454e904d7e68e2
CCP Cookies <69311849+ccpcookies@users.noreply.github.com>
2023-12-08 09:39:06 +0000

    Add requirement for macOS 10.5 or greater, show error dialog on OS condition failed

--- ExeFileOsX.mm
+++ ExeFileOsX.mm
@@ -43,10 +43,22 @@ void SetProcessAffinity( int affinity )
 {
 }
 
+bool IsSupportedOSVersion()
+{
+    if( @available( macOS 10.5, * ) )
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
 int main( int argc, char* argv[] )
 {
 	CommandLine commandLine = ParseCommandLine( argc, argv );
-	return Main( commandLine );
+	return Main( commandLine, IsSupportedOSVersion() );
 }
 
 #endif
