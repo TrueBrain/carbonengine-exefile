df8316efc28daa05a0106d3e64bb4302b5a0bbae
CCP Toebeans <105929497+ccptoebeans@users.noreply.github.com>
2025-06-19 10:52:49 +0000

    Remove crashpad build type constraint from osx triplet file

--- cmake/triplets/universal-osx-carbon.cmake
+++ cmake/triplets/universal-osx-carbon.cmake
@@ -5,6 +5,3 @@ set(VCPKG_LIBRARY_LINKAGE dynamic)
 set(VCPKG_CMAKE_SYSTEM_NAME Darwin)
 set(VCPKG_OSX_ARCHITECTURES "arm64;x86_64")
 
-if(PORT MATCHES "crashpad")
-    set(VCPKG_BUILD_TYPE release)
-endif()
